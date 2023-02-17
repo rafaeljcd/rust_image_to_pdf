629c62b47cb302cea5aa9d7003fa49b4d13b5b92
rafaeljcd <123484899+rafaeljcd@users.noreply.github.com>
2023-02-17 20:04:13 +0800

    Update main.rs
    printed out the current directory

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,11 @@
-use std::env::consts;
+use std::env::{consts, current_dir};
 fn main() {
     // print out the OS version
-    println!("{}", consts::OS);
+    let os = consts::OS;
+    println!("{}", os);
+
+    let cwd = current_dir()
+        .expect("Error getting the current directory ");
+
+    println!("current directory: {}", cwd.to_string_lossy());
 }
