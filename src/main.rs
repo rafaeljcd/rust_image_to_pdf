ea62bb44d97a77825225023258ce0938fc571975
rafaeljcd <123484899+rafaeljcd@users.noreply.github.com>
2023-02-17 20:39:15 +0800

    Update main.rs
    cannot use the `?` operator in a function that returns `()`

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,20 @@
 use std::env::{consts, current_dir};
+use std::fs::read_dir;
 fn main() {
     // print out the OS version
     let os = consts::OS;
     println!("{}", os);
 
-    let cwd = current_dir()
-        .expect("Error getting the current directory ");
+    let cwd = current_dir().expect("Error getting the current directory ");
 
     println!("current directory: {}", cwd.to_string_lossy());
+
+    // convert pathbuf to path
+    let cwd_path = cwd.as_path();
+
+    for entry in read_dir(cwd_path)? {
+        let dirEntry = entry.expect("Error directory");
+
+        println!("entry {}", dirEntry.file_name().to_string_lossy());
+    }
 }
