7d7e67234c67aefdffa7109e239d9d3002c24b7c
rafaeljcd <123484899+rafaeljcd@users.noreply.github.com>
2023-02-17 21:56:37 +0800

    resolve the error with Result from read_dir

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,25 @@
 use std::env::{consts, current_dir};
-use std::fs::read_dir;
-fn main() {
+use std::fs::{self};
+use std::io::Error as IOError;
+
+fn main() -> Result<(), IOError>{
     // print out the OS version
     let os = consts::OS;
     println!("{}", os);
 
-    let cwd = current_dir().expect("Error getting the current directory ");
+    let cwd = match current_dir() {
+        Ok(path_buf) => path_buf,
+        Err(e) => panic!("error {}", e.to_string()),
+    };
 
     println!("current directory: {}", cwd.to_string_lossy());
 
     // convert pathbuf to path
     let cwd_path = cwd.as_path();
 
-    for entry in read_dir(cwd_path)? {
-        let dirEntry = entry.expect("Error directory");
-
-        println!("entry {}", dirEntry.file_name().to_string_lossy());
+    for entry in fs::read_dir(cwd_path)? {
+        let entry_dir = entry?;
     }
+
+    Ok(())
 }
